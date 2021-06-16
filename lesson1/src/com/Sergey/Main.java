0a2534ba42df6544b308ac3352f53b7d776b298a
lesson1 <s.v.kurtsev@gmail.com>
2021-06-16 21:33:42 +0300

    Fourth commit for the second branch

--- lesson1/src/com/Sergey/Main.java
+++ lesson1/src/com/Sergey/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println("Test1234567");
+        System.out.println("Second_branch");
     }
 }
